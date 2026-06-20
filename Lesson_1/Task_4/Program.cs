334d16e6da0dbb1fabd6e330412230894559ef27
Stanislav krasko <staskrasko1991krasko@gmail.com>
2026-06-20 12:03:13 +0300

    add Lesson_1 Task_4

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 31;
+int b = 8;
+int c = 91;
+int d = 5;
+int e = 11;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max )
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max) 
+{
+    max = e;
+}
+
+Console.WriteLine(max);
